97fa7a58ce044e48c97f3244d50dee3f941abdda
Tyler Reinecke <Guest@WTS12.local>
2015-07-12 18:58:41 -0700

    Summary
    Description

---
+++ IntroToJava/src/SecretMessageBox.java
@@ -0,0 +1,37 @@
+// Copyright Wintriss Technical Schools 2014
+import javax.swing.JOptionPane;
+
+/**
+ * Secret Message Box / Secure Messaging System
+ * 
+ * You want to leave a message on one of the Mac computers so that only your friend can read it.
+ * You set up the passcode and the secret message. 
+ * Your friend types in the passcode to retrieve the message.
+ * 
+ */
+
+public class SecretMessageBox {
+
+	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args)
+	{
+		// 1. Set the passcode in a String variable
+		String passcode = "password";	
+		// 2. Using a pop-up, ask for a secret message and store it in a variable
+		String scrtMess = JOptionPane.showInputDialog("Type a secret message here.");
+		// 3. Ask your friend for the passcode and store it in a variable
+		String passAttempt = JOptionPane.showInputDialog("What is the passcode?");
+		// 4. If the passcode matches, show the secret message
+		if(passAttempt.equals(passcode))
+		{
+			JOptionPane.showMessageDialog(null, scrtMess);
+		}
+		// 5. If the passcode does not match, pop-up "INTRUDER!!"
+		if(!(passAttempt.equals(passcode)))
+		{
+			JOptionPane.showMessageDialog(null, "INTRUDER!!");
+		}
+	}
+}
+
+
